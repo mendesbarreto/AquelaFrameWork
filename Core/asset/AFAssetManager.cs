390c45d8b4259fc4782291bceb1a369a3ae7693f
Douglas Mendes Barreto <mendes-barreto@live.com>
2014-08-15 20:40:01 -0300

    Add New Class AFPool

---
+++ Core/asset/AFAssetManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic
+using UnityEngine;
+
+using AFFrameWork.Sound;
+
+namespace AFFrameWork.Core
+{
+
+    public class AFAssetManager : ASingleton<AFAssetManager>
+    {
+        protected Dictionary<string, Texture> m_textures = new Dictionary<string,Texture>();
+
+        protected Dictionary<string , AFSound> m_sounds = new Dictionary<string,AFSound>();
+
+        protected Dictionary<string , GameObject> m_prefabs = new Dictionary<string,GameObject>();
+
+        protected Dictionary<string , object> m_custom = new Dictionary<string,object>();
+
+        
+        public Texture GetTexture( string name )
+        {
+            if( m_textures.ContainsKey( name ) )
+            {
+                return m_textures[name];
+            }
+            
+            UnityEngine.Debug.LogWarning("Texture " + name + " was not found!");
+
+
+            return null;
+        }
+
+        public GameObject GetPrefabs( string name )
+        {
+            if( m_prefabs.ContainsKey( name ) )
+            {
+                return m_prefabs[name];
+            }
+            
+            UnityEngine.Debug.LogWarning("Prefeb: " + name + " was not found!");
+
+
+            return null;
+        }
+
+        public AFSound GetAFSound( string name )
+        {
+            if( m_sounds.ContainsKey( name ) )
+            {
+                return m_sounds[name];
+            }
+            
+            UnityEngine.Debug.LogWarning(" AFSound: " + name + " was not found!");
+
+
+            return null;
+        }
+
+        public AFSound GetAFSound( string name )
+        {
+            if( m_sounds.ContainsKey( name ) )
+            {
+                return m_sounds[name];
+            }
+            
+            UnityEngine.Debug.LogWarning(" AFSound: " + name + " was not found!");
+
+
+            return null;
+        }
+
+
+
+    }
+}
---
+++ Core/asset/AFPool.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+using UnityEngine;
+
+namespace AFFrameWork.Core.asset
+{
+    public class AFPool<T> where T : object
+    {
+        private string m_name = "";
+        private List<T> m_pooledObjs = new List<T>();
+        
+        private uint m_qtdObjectsPooled = 0;
+        private uint m_maxObjects = 0;
+
+        public AFPool( string name , uint maxObjects = 0)
+        {
+            m_name = name;
+            m_maxObjects = maxObjects;
+        }
+
+        public void Add( T obj )
+        {
+            if (m_maxObjects > 0 && m_qtdObjectsPooled <= m_maxObjects)
+            { 
+                m_pooledObjs.Add(obj);
+                m_qtdObjectsPooled++;
+            }
+            else
+            {
+                UnityEngine.Debug.LogWarning("The object could not be added, because the pool reached your max limit of: " + m_maxObjects);
+            }
+        }
+
+        public void Destroy()
+        {
+            m_qtdObjectsPooled = 0;
+            m_pooledObjs.Clear();
+            m_pooledObjs = null;
+        }
+
+
+    }
+}
