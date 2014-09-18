15b273e606e250529d155aa35531c34b053171da
Douglas Mendes Barreto <mendes-barreto@live.com>
2014-09-18 18:52:08 -0300

    Criado com sucesso o controlador de estados da animação

---
+++ View/AFStatesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+using AFFrameWork.Core;
+
+
+namespace AFFrameWork.View
+{
+    public class AFStatesController : AFObject , IAnimatable
+    {
+        protected int m_defaultStateID = 0;
+        protected int m_currentStateID = 0;
+        protected int m_lastStateID = 0;
+        protected int m_nextStateID = 0;
+
+        protected AFMovieClip m_currentState;
+
+        protected Dictionary<int, AFMovieClip> m_states;
+        protected bool m_update = true;
+
+        public AFMovieClip GetState(string name) { return GetState(name.GetHashCode()); }
+        public AFMovieClip GetState(int stateID) 
+        {
+            if (m_states.ContainsKey(stateID)) UnityEngine.Debug.LogWarning("The state was not here");
+            return m_states[stateID]; 
+        }
+
+        private AFStatesController()
+        {
+            m_states = new Dictionary<int, AFMovieClip>();
+        }
+
+        public void Add( string name , AFMovieClip state , bool defaultState = false)
+        {
+            Add(name.GetHashCode(), state, defaultState);
+        }
+
+        public void Add(int name, AFMovieClip state, bool defaultState = false)
+        {
+            if (m_states.ContainsKey(name)) throw new Exception("The State " + name + " already was created");
+
+            state.transform.parent = this.gameObject.transform;
+
+            if (!defaultState)
+            {
+                state.Stop();
+            }
+            else
+            {
+                if (m_defaultStateID != 0)
+                {
+                    UnityEngine.Debug.LogWarning("The default state was changed to <" + name + ">");
+                }
+
+                m_defaultStateID = name;
+                state.Play();
+            }
+
+
+            state.gameObject.SetActive(defaultState);
+            m_currentState = state;
+            m_currentStateID = name;
+            m_states.Add(name, state);
+        }
+
+        public void AdvanceTime(double time)
+        {
+            if (!m_update || !this.gameObject.activeSelf) return;
+
+            if( m_nextStateID != 0 )
+            {
+                m_currentState.Stop();
+                m_currentState.gameObject.SetActive(false);
+
+                m_lastStateID = m_currentStateID;
+                m_currentStateID = m_nextStateID;
+                m_currentState = m_states[m_nextStateID];
+                m_currentState.Play();
+                m_currentState.gameObject.SetActive(true);
+
+                m_nextStateID = 0;
+            }
+            
+            m_currentState.AdvanceTime(time);
+        }
+
+        public void Remove( string name )
+        {
+            Remove(name.GetHashCode());
+        }
+
+        public void Remove(int stateID)
+        {
+            if (m_states.ContainsKey(stateID))
+            {
+                m_states.Remove(stateID);
+
+                if (stateID == m_defaultStateID)
+                {
+                    m_defaultStateID = m_states.Keys.First() != null ? m_states.Keys.First() : 0;
+                    UnityEngine.Debug.LogWarning("You are removing a default state. The default state was changed for the first state in the list. ID: " + m_defaultStateID);
+                }
+
+                if (stateID == m_currentStateID)
+                {
+                    GoToDefault();
+                }
+            }
+        }
+
+        public void Play()
+        {
+            m_update = true;
+            m_currentState.Play();
+        }
+
+        public void Pause()
+        {
+            m_update = false;
+        }
+
+        public void Stop()
+        {
+            m_update = false;
+            m_currentState.Stop();
+        }
+
+        public void GoTo( string name )
+        {
+            ChangeStateTo(name.GetHashCode());
+        }
+
+        public void GoTo(int hashName)
+        {
+            ChangeStateTo(hashName);
+        }
+
+
+        public void GoToDefault()
+        {
+            ChangeStateTo(m_defaultStateID);
+        }
+
+        private void ChangeStateTo( int stateID )
+        {
+            if (stateID == m_currentStateID) return;
+            if ( !m_states.ContainsKey( stateID ) ) throw new Exception("State ID not valid");
+
+            m_nextStateID = stateID;
+        }
+
+        public int GetDefaultStateID()
+        {
+            return m_defaultStateID;
+        }
+
+        public AFMovieClip GetDefaultState()
+        {
+            return m_states[m_defaultStateID];
+        }
+
+        public AFMovieClip GetLastState()
+        {
+            return m_states[m_lastStateID];
+        }
+
+        public AFMovieClip GetCurrentState()
+        {
+            return m_states[m_currentStateID];
+        }
+        public int GetCurrentStateID()
+        {
+            return m_currentStateID;
+        }
+
+        public int GetLastStateID()
+        {
+            return m_lastStateID;
+        }
+
+    }
+}
