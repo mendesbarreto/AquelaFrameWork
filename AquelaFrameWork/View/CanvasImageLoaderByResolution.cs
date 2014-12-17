f3b77190177eedcd40416f01230375fd0535843e
Douglas Mendes Barreto <mendes-barreto@live.com>
2014-12-17 13:56:22 -0200

    Fix: Assetmanager não estava salvando corretamente as imagens, AFTextureAtlas estava com problema de carregamento e parse das imagens

---
+++ AquelaFrameWork/View/CanvasImageLoaderByResolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using AquelaFrameWork.Core;
+using AquelaFrameWork.Core.Asset;
+
+namespace AquelaFrameWork.View
+{
+    public class CanvasImageLoaderByResolution : AFObject
+    {
+        public string path = "Scenes/Login/Background";
+
+        void Start()
+        {
+            Sprite sp = AFAssetManager.Instance.Load<Sprite>(AFAssetManager.GetPathTargetPlatformWithResolution() + "/" + path);
+            Image L_img = gameObject.GetComponent<Image>();
+
+            if (L_img != null)
+            {
+                L_img.sprite = sp;
+            }
+        }
+
+    }
+}
